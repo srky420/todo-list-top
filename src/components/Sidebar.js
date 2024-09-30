2791ea331c4fcce74ddecb7b787260236a595511
srky420 <srky420@gmail.com>
2024-09-30 17:48:13 +0500

    Add styling for sidebar scroll

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,15 +1,20 @@
 class Sidebar {
   #sidebar;
   #sidebarOpen;
+  #sidebarIcons;
+  #overlay;
   
   #cacheDOM() {
     this.#sidebar = document.querySelector("aside");
     this.#sidebarOpen = document.querySelector(".aside-open-btn");
+    this.#sidebarIcons = this.#sidebarOpen.querySelectorAll("i");
+    this.#overlay = document.querySelector(".sidebar-overlay");
   }
 
   #addEventListeners() {
     this.#sidebarOpen.addEventListener("click", () => {
       this.#sidebar.classList.toggle("aside-open");
+      this.#sidebarIcons.forEach(i => i.classList.toggle("hidden"));
     });
   }
 
